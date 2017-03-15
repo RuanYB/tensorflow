d84a067f9c8d968c9be78f888ddbbfc2fa51d34e
ryan <wufazhuceryan@yeah.net>
2017-03-15 16:48:07 +0800

    complete cifar10_input

--- cifar10/cifar10_input.py
+++ cifar10/cifar10_input.py
@@ -114,7 +114,7 @@ def _generate_image_and_label_batch(image, label, min_queue_examples,
 
 
 def distorted_inputs(data_dir, batch_size):
-	"""使用Reader op为CIFAR训练构造augment data
+	"""使用Reader op为CIFAR训练环节构造augment data
 	参数Args：
 		data_dir：CIFAR-10数据目录路径
 		batch_size：每个batch中的图像样本数
@@ -133,6 +133,95 @@ def distorted_inputs(data_dir, batch_size):
 
 	#从filename_queue队列中的文件中读取样本
 	read_input = read_cifar10(filename_queue)
-	reshape_image = tf.cast(read_input.uint8image, tf.float32)
+	reshaped_image = tf.cast(read_input.uint8image, tf.float32)
 
+	height = IMAGE_SIZE
+	width = IMAGE_SIZE
+
+	#处理用于训练的图像，对图像作不同的变形
 	
+	#图像随机裁剪成[height, width]的大小
+	distorted_image = tf.random_crop(reshaped_image, [height, width, 3])
+
+	#随机水平翻转图像
+	distorted_image = tf.image.random_flip_left_right(distorted_image)
+
+	#随机改变亮度和对比度
+	distorted_image = tf.image.random_brightness(distorted_image, 
+												max_delta=63)
+	distorted_image = tf.image.random_contrast(distorted_image, 
+												lower=0.2, upper=1.8)
+
+	#标准化：每个像素减去平均值，除以方差
+	float_image = tf.image.per_image_standardization(distorted_image)
+
+	#调整tensor的shape
+	float_image.set_shape([height, width, 3])
+	read_input.label.set_shape([1])
+
+	#确保随机打乱有着良好的混合性质
+	min_fraction_of_examples_in_queue = 0.4
+	min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * 
+							min_fraction_of_examples_in_queue)
+	print('Filling queue with %d CIFAR images before starting to train'
+			'This will take a few minutes.' % min_queue_examples)
+
+	#创建样本队列，生成一个batch的images & labels
+	return _generate_image_and_label_batch(float_image, read_input.label, 
+											min_queue_examples, batch_size, 
+											shuffle=True)
+
+
+def inputs(eval_data, data_dir, batch_size):
+	"""使用Reader op为CIFAR评价环节构造输入
+	参数Args：
+		eval_data:布尔值，代表是否使用训练或者评价数据集
+		data_dir:CIFAR-10数据目录路径
+		batch_size:每一个batch的图像数
+	返回Returns：
+		images:Images. 4维tensor，shape[batch_size, IMAGE_SIZE, IMAGE_SIZE, 3]
+		labels:Labels. 1维tensor
+	"""
+	if not eval_data:
+		filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i)
+					for i in xrange(1,6)]
+		num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+	else:
+		filenames = [os.path.join(data_dir, 'test_batch.bin')]
+		num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
+
+	for f in filenames:
+		if not tf.gfile.Exists(f):
+			raise ValueError('Failed to find file: ' + f)
+
+	#创建一个队列用于读取文件名
+	filename_queue = tf.train.string_input_producer(filenames)
+
+	#从filename队列的文件中读取样本
+	read_input = read_cifar10(filename_queue)
+	reshaped_image = tf.cast(read_input.uint8image, tf.float32)
+
+	height = IMAGE_SIZE
+	width = IMAGE_SIZE
+
+	#处理用于评价的图像
+	#图像中心裁剪为[height, width]的大小
+	resized_image = tf.image.resize_image_with_crop_or_pad(reshaped_image, 
+															width, height)
+
+	#每个像素减去平均值，除以方差
+	float_image = tf.image.per_image_standardization(resized_image)
+
+	#调整tensor的shape
+	float_image.set_shape([height, width, 3])
+	read_input.label.set_shape([1])
+
+	#确保随机打乱有着良好的性质
+	min_fraction_of_examples_in_queue = 0.4
+	min_queue_examples = int(num_examples_per_epoch *
+							min_fraction_of_examples_in_queue)
+
+	#创建样本队列，生成一个batch的images & labels
+	return _generate_image_and_label_batch(float_image, read_input.label,
+											min_queue_examples, batch_size,
+											shuffle=False)
